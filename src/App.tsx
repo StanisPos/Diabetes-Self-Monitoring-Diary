8dd99dce99c5c67d5534171b5fbde8807627ff8a
Stanislav Posidelov <sposidelov@roox.ru>
2020-04-24 00:24:15 +0300

    Removed styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,31 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store/store';
+import MeasureItem from './components/measure-item';
 
 type TemplateType = {
-    title: string;
-    status: string;
-}
+	title: string,
+	status: string,
+};
 
 const NavBar = () => (
-    <div className="navbar">
-        <h3>Task Manager</h3>
-        <Link to="/">Current Tasks</Link>
-        <Link to="/completed">Completed Tasks</Link>
-    </div>
+	<div className="navbar">
+		<h3>Task Manager</h3>
+		<Link to="/">Current Tasks</Link>
+		<Link to="/completed">Completed Tasks</Link>
+	</div>
 );
 
 const Template: React.FC<TemplateType> = ({ title, status }) => (
-    <div>
-        <NavBar />
-        <p className="page-info">{title}:</p>
-        <ul className={status}>
-            <li>Task 1</li>
-            <li>Task 2</li>
-            <li>Task 3</li>
-        </ul>
-    </div>
+	<div>
+		<NavBar />
+		<p className="page-info">{title}:</p>
+		<ul className={status}>
+			<li>Task 1</li>
+			<li>Task 2</li>
+			<li>Task 3</li>
+		</ul>
+	</div>
 );
 
 const CurrentTasks = () => <Template title="Current Tasks" status="Current" />;
@@ -34,20 +35,21 @@ const CurrentTasks = () => <Template title="Current Tasks" status="Current" />;
 const CompletedTasks = () => <Template title="Completed Tasks" status="Completed" />;
 
 class App extends PureComponent {
-    render() {
-        return (
-            <Provider store={store}>
-                <PersistGate loading={null} persistor={persistor}>
-                    <BrowserRouter>
-                        <div>
-                            <Route exact path="/" component={CurrentTasks} />
-                            <Route path="/completed" component={CompletedTasks} />
-                        </div>
-                    </BrowserRouter>
-                </PersistGate>
-            </Provider>
-        );
-    }
+	render() {
+		return (
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<BrowserRouter>
+						<div>
+							<Route exact path="/" component={CurrentTasks} />
+							<Route path="/completed" component={CompletedTasks} />
+							<MeasureItem />
+						</div>
+					</BrowserRouter>
+				</PersistGate>
+			</Provider>
+		);
+	}
 }
 
 export default App;
